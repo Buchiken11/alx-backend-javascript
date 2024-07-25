7257ef46187c79bc3f61dc7b7bd972c162ff0105
Buchiken11 <buchiken025@gmail.com>
2024-07-25 05:21:54 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-	return promise
-		.then((resolve) => {
-			return { status: 200, body: "success"};
-		})
-		.catch((Error) => {
-			return (new Error());
-		})
-		.finally(() => {
-			console.log("Got a response from the API");
-		})
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
